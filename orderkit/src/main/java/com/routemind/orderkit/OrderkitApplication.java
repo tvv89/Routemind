8611c763820f3ab7cc3fee647918fab2b024baea
tymchuk.volodymyr <tymchuk.volodymyr@gmail.com>
2023-10-13 00:22:15 +0300

    RM-OK: Create project

---
+++ orderkit/src/main/java/com/routemind/orderkit/OrderkitApplication.java
@@ -0,0 +1,13 @@
+package com.routemind.orderkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderkitApplication.class, args);
+    }
+
+}
